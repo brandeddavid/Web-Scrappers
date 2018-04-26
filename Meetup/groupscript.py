183e6a71f9a76d7af84033bc155b48a0627df823
David Mwangi Mathenge <david.mathenge98@gmail.com>
2018-04-26 23:17:55 +0300

    Refactor code

--- Meetup/groupscript.py
+++ Meetup/groupscript.py
@@ -1,47 +1,41 @@
 from urllib.request import urlopen
 import csv, json
 
-offset = 0
+def groups(category, key):
+    offset = 0
+    while offset < 22:
+        print('Scrapping offset:', offset)
+        url = 'https://api.meetup.com/2/groups?&sign=true&photo-host=public&topic='+category+'&page=200&offset=' + str(offset) + '&key=' + key 
+        #2317179606b581356403e49598658
+        #page = urlopen(url).read().decode('utf-8')
+        with urlopen(url) as url:
+            data = json.loads(url.read().decode())
+            with open (category + '.csv', 'a', newline='') as file:
+                headers = ['Name', 'Members', 'Link', 'Country', 'State']
+                writer = csv.DictWriter(file, fieldnames=headers)
+                for group in data['results']:
+                    try:
+                        name = group['name']
+                        members = group['members']
+                        link = group['link']
+                        country = group['country']
+                        state = group['state']
+                    except:
+                        pass
+                    finally:
+                        writer.writerow({'Name':name, 'Members':members, 'Link':link, 'Country':country, 'State':state})
+                        name = ''
+                        members = ''
+                        link = ''
+                        country = ''
+                        state = ''
+        offset += 1
 
-while offset < 22:
 
-    print('Scrapping offset:', offset)
+if __name__ == '__main__':
 
-    url = 'https://api.meetup.com/2/groups?&sign=true&photo-host=public&topic=cryptocurrency&page=200&offset=' + str(offset) + '&key=2317179606b581356403e49598658'
-
-    links = []
-    #page = urlopen(url).read().decode('utf-8')
-    with urlopen(url) as url:
-
-        data = json.loads(url.read().decode())
-
-        with open ('cryptogroups.csv', 'a') as file:
-
-            headers = ['Name', 'Members', 'Link', 'Country', 'State']
-
-            writer = csv.DictWriter(file, fieldnames=headers)
-
-            for group in data['results']:
-
-                try:
-
-                    name = group['name']
-                    members = group['members']
-                    link = group['link']
-                    country = group['country']
-                    state = group['state']
-
-                except:
-
-                    pass
-
-                finally:
-
-                    writer.writerow({'Name':name, 'Members':members, 'Link':link, 'Country':country, 'State':state})
-                    name = ''
-                    members = ''
-                    link = ''
-                    country = ''
-                    state = ''
-
-    offset += 1
+    print("Enter Search Category:")
+    category = input(">")
+    print("Enter Key:")
+    key = input(">")
+    groups(category,key)
--- Meetup/memberscript.py
+++ Meetup/memberscript.py
@@ -1,50 +1,53 @@
 from urllib.request import urlopen
 import csv, json, math
+import time
 
 
-group = '4-Women-Entrepreneurs'
+def groupNames(file):
+    groups = []
+    with open(file, 'r') as infile:
+        reader = csv.reader(infile, delimiter=',')
+        for row in reader:
+            try:
+                groups.append(row[2].split('/')[-2])
+            except:
+                pass
+        return groups
 
-urlj = 'https://api.meetup.com/2/members?&sign=true&photo-host=public&group_urlname=' + group + '&page=20&key=2317179606b581356403e49598658'
-with urlopen(urlj) as urlj:
+def groupMembers(groups):
+    for group in groups:
+        urlj = 'https://api.meetup.com/2/members?&sign=true&photo-host=public&group_urlname=' + group + '&page=20&key=2317179606b581356403e49598658'
+        with urlopen(urlj) as urlj:
+            jsn = json.loads(urlj.read().decode())
+            total_count = jsn['meta']['total_count']
+            count = math.ceil(total_count/200)
+        offset = 0
+        while offset < count:
+            print('Offset:', offset)
+            url = 'https://api.meetup.com/2/members?&sign=true&photo-host=public&group_urlname=' + group + '&page=200&offset=' + str(offset) + '&key=2317179606b581356403e49598658'
+            with urlopen(url) as url:
+                data = json.loads(url.read().decode())
+                with open('members.csv', 'a', newline='') as file:
+                    headers = ['Name', 'Country', 'City', 'Link', 'Group', 'Status']
+                    writer = csv.DictWriter(file, fieldnames=headers)
+                    for person in data['results']:
+                        try:
+                            name = person['name']
+                            country = person['country']
+                            city = person['city']
+                            link = person['link']
+                            status = person['status']
+                        except:
+                            pass
+                        finally:
+                            writer.writerow({'Name':name, 'Country':country, 'City':city, 'Link':link, 'Group':group, 'Status':status})
+            offset += 1
 
-    jsn = json.loads(urlj.read().decode())
 
-    total_count = jsn['meta']['total_count']
-
-    count = math.ceil(total_count/200)
-
-offset = 0
-
-while offset < count:
-
-    print('Offset:', offset)
-
-    url = 'https://api.meetup.com/2/members?&sign=true&photo-host=public&group_urlname=' + group + '&page=200&offset=' + str(offset) + '&key=2317179606b581356403e49598658'
-
-    with urlopen(url) as url:
-
-        data = json.loads(url.read().decode())
-
-        with open('members.csv', 'a', newline='') as file:
-
-            headers = ['Name', 'Country', 'City', 'Link', 'Group', 'Status']
-            writer = csv.DictWriter(file, fieldnames=headers)
-
-            for person in data['results']:
-
-                try:
-
-                    name = person['name']
-                    country = person['country']
-                    city = person['city']
-                    link = person['link']
-                    status = person['status']
-
-                except:
-
-                    pass
-
-                finally:
-
-                    writer.writerow({'Name':name, 'Country':country, 'City':city, 'Link':link, 'Group':group, 'Status':status})
-    offset += 1
+if __name__ == '__main__':
+    file = input("Enter file name:\n>")
+    groupsList = groupNames(file)
+    time.sleep(5)
+    # print(groupsList)
+    # print(len(groupsList))
+    groupMembers(groupsList)
