33af9b95a7f48d01b4f8d0978cb2fc4383321ce2
David Mwangi Mathenge <david.mathenge98@gmail.com>
2018-05-09 08:14:26 +0300

    Add location and repo url

--- GitHub/gitRankScrapper.py
+++ GitHub/gitRankScrapper.py
@@ -1,19 +1,36 @@
+"""[summary]
+
+Returns:
+    [type] -- [description]
 """
-"""
+
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from github import Github
 import csv 
 
 def login(username, password):
+    """[summary]
+    
+    Arguments:
+        username {[type]} -- [description]
+        password {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
     """
-    """
+
     githubInstance = Github(username, password)
     return githubInstance
 
 def scrapper(url):
+    """[summary]
+    
+    Arguments:
+        url {[type]} -- [description]
     """
-    """
+
     github = login('brandeddavid', 'marigi@98')
     page_html = urlopen(url).read()
     soup = BeautifulSoup(page_html, 'html.parser')
@@ -21,7 +38,7 @@ def scrapper(url):
     trs = table[0].findAll('tr')
     print (trs[0])
     with open('ChinaTop1000.csv', 'a', newline='') as outfile:
-        headers = ['Name', 'Bio', 'Email', 'Site', 'Followers', 'Repos', 'Languages', 'Link']
+        headers = ['Name', 'Bio', 'Location', 'Email', 'Site', 'Followers', 'Repos', 'Languages', 'Link']
         writer = csv.DictWriter(outfile, fieldnames=headers)
         writer.writeheader()
         for tr in trs:
@@ -32,13 +49,14 @@ def scrapper(url):
                 user = github.get_user(username)
                 github_name = user.name
                 github_bio = user.bio
+                github_location = user.location
                 github_email = user.email
                 github_site = user.blog
                 github_followers = user.followers
-                github_pop_repo = None
+                github_repo = user.repos_url
                 github_languages = tds[5].text
                 github_link = user.html_url
-                writer.writerow({'Name': github_name, 'Bio': github_bio, 'Email': github_email, 'Site': github_site, 'Followers': github_followers, 'Repo': github_pop_repo, 'Languages': github_languages, 'Link': github_link})
+                writer.writerow({'Name': github_name, 'Bio': github_bio, 'Location':github_location, 'Email': github_email, 'Site': github_site, 'Followers': github_followers, 'Repo': github_repo, 'Languages': github_languages, 'Link': github_link})
             except:
                 pass
             finally:
@@ -48,7 +66,7 @@ def scrapper(url):
                 github_email = None
                 github_site = None
                 github_followers =  None
-                github_pop_repo = None
+                github_repo = None
                 github_languages = None
                 github_link = None
 
