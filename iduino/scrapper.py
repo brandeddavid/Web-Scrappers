0899570bf85ae0374927b644f37c0dae4e11bfde
David Mwangi <david.mathenge98@gmail.com>
2019-03-03 07:13:34 +0300

    change product fields clearing order

--- iduino/scrapper.py
+++ iduino/scrapper.py
@@ -5,16 +5,17 @@ import csv, wget, time
 base_url = 'http://www.openplatform.cc'
 page = 1
 
-with open('products.csv', 'a', newline='') as file:
+with open('iduino-board.csv', 'a', newline='') as file:
   headers = ['Name', 'Number', 'Image', 'Page', 'Description']
   writer = csv.DictWriter(file, fieldnames=headers)
   writer.writeheader()
 
-  while page < 23:
-    print('\nScraping page\n', page)
+  while page < 12:
+    print('\nScraping page', page)
 
     try:
-      url = 'http://www.openplatform.cc/index.php/Home/Index/products/p/' + str(page) + '.html'
+      url = 'http://www.openplatform.cc/index.php/Home/Index/products/aptid/7/p/' + str(page) + '.html'
+      # url = 'http://www.openplatform.cc/index.php/home/index/products/aptid/26/apiinfor/'
       url_html = urlopen(url).read()
       soup = BeautifulSoup(url_html, 'html.parser')
         
@@ -22,12 +23,14 @@ with open('products.csv', 'a', newline='') as file:
         product_div = soup.findAll('div', {'class': 'searListsub'})
 
         for product in product_div:
+          print('\nProduct ' + str(product_div.index(product)+1) + ' of ' + str(len(product_div)))
+
           try:
             product_name = product.findAll('h5', {'class': 'itemTitle'})[0].a.text
             product_no = product.findAll('div', {'class': 'c9'})[0].text
             product_image = base_url + product.findAll('img', {'class': 'img-responsive'})[0]['src']
             wget.download(product_image, './' + product_name + '.jpg')
-            time.sleep(15)
+            time.sleep(5)
             product_url = base_url + product.findAll('a', {'class': 'thumb'})[0]['href']
 
             try:
@@ -44,8 +47,12 @@ with open('products.csv', 'a', newline='') as file:
             pass
 
           finally:
-            pass
-            writer.writerow({'Name': product_name, 'Number': product_no, 'Image': product_image, 'Page': product_url, 'Description': product_description})
+             writer.writerow({'Name': product_name, 'Number': product_no, 'Image': product_image, 'Page': product_url, 'Description': product_description})
+            product_name = ''
+            product_no = ''
+            product_image = ''
+            product_url = ''
+            product_description = ''
       
       except Exception as error:
         print('Product list', error)
@@ -56,7 +63,3 @@ with open('products.csv', 'a', newline='') as file:
 
     finally:
       page += 1
-      product_name = ''
-      product_no = ''
-      product_image = ''
-      product_url = ''
